c92971132545e43af1ffd866577d60901acbe902
Eros Pingani <e.pingani3@gmail.com>
2021-01-09 11:04:27 +0100

    [DEV][BE] - Added implementation for hashkey check and error management in newMail service

--- longobardiBACK.rb
+++ longobardiBACK.rb
@@ -1,5 +1,13 @@
 require 'sinatra'
+require 'digest'
 require 'json'
+require 'active_support/json'
+
+KEY = '196B5815CDE73CAE5CD7018359B851BBA754F80BB36FB7D50C1650F012496F70'
+
+def checkHashKey(hashkey, date, time, battery)
+    return (Digest::SHA256.hexdigest KEY + date + time + battery).downcase == hashkey.downcase ? true : false
+end
 
 class Mail
     attr_accessor :date, :time, :battery, :description
@@ -8,23 +16,38 @@ end
 class LongobardiBACK < Sinatra::Base
     newMail = false
     mail = Mail.new()
-    puts('First run, available inboxes? ', newMail)
+    #Called by ESP8266 when the sensor is triggered
     post '/newMail' do
-        newMail = true
-        mail.date = params['date']
-        mail.time = params['time']
-        mail.battery = params['battery']
-        puts('Hey, there\'s a new Inbox!')
-        # Aggiungere gestione response
+        response = {}
+        hashkey = params['hashkey']
+        date = params['date']
+        time = params['time']
+        battery = params['battery']
+        if (checkHashKey(hashkey, date, time, battery)) #The hashkey is checked to avoid data manipulation from unknown sources
+            newMail = true
+            mail.date = date
+            mail.time = time
+            mail.battery = battery
+            response['status'] = 'ok'
+            response['message'] = 'New mail registered'
+        else
+            response['status'] = 'ko'
+            response['message'] = 'An error occured, please try again'
+        end
+        content_type :json
+        response.to_json
     end
+    #Called by longobardicAPP to check whether a new mail is available
     get '/checkMailbox' do
         response = {}
         if (newMail) 
-            response['available'] = true
-            response['mail'] = mail
+            response['status'] = 'ok'
+            response['available_inbox'] = true
+            response['mail'] = JSON.parse(ActiveSupport::JSON.encode(mail)) #Hashed object converted in JSON
             newMail = false
         else 
-            response['available'] = false
+            response['status'] = 'ko'
+            response['available_inbox'] = false
         end
         content_type :json
         response.to_json

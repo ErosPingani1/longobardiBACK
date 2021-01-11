99b59e98103a496a4a4e517955086e00819ce275
Eros Pingani <e.pingani3@gmail.com>
2021-01-11 22:29:42 +0100

    [DEV][BE] - Added Firebase Cloud Messaging library and first sendPushNotification implementation

--- longobardiBACK.rb
+++ longobardiBACK.rb
@@ -1,12 +1,29 @@
 require 'sinatra'
+require 'fcm'
 require 'digest'
 require 'json'
 require 'active_support/json'
 
 KEY = '196B5815CDE73CAE5CD7018359B851BBA754F80BB36FB7D50C1650F012496F70'
 
+#Firebase Cloud Messaging Server Key
+$fcm = FCM.new('AAAA0ID3FSw:APA91bHveLJLI9jU_M3GQAN1ej-H8DjOFtxbFwGJ9kqWD2Ct8m1KoMc9FoTS9QAeQo62nj_PJ6jmkEvlGH0nmPqSIwpo7bZaEN0tXNUWCXakM-D6G9UGhphAmB8KVAFd1AmO-09WeI2E')
+#longobardicAPP registration token taken at opening from AStudio console (emulator, Pixel 3a)
+$registration_ids = ['e07mwn9JTCqIhKCkDFOr9g:APA91bEAwliA2uFbIP9pOFh8XlZ96R6S_jyQL1SapZK3SaxOhgQ6r2WGfuRoFRQRZhAmGbBdikc5vYChWwtpeMd9hIYCb3VsIjQxAYXsq1CLpUM-08eeHEsCX_lRNn_wVRe47F2shjJe', 'fOhLXUDkT9q5Sy_zA334I3:APA91bFfS8arvQgx4Po0DPIx26_ayYE_q8_4y37LLrj88-VgrFFIEce9fHmUNMozhSnSACWrE7sWvqCsWtMeRprKlfSLOQaWHgHht4UJ6BLV-L-YKkuXFw-PeANifxXB_CbFd8Cz3cBY']
+
 def checkHashKey(hashkey, date, time, battery)
     return (Digest::SHA256.hexdigest KEY + date + time + battery).downcase == hashkey.downcase ? true : false
+end
+
+def sendPushNotification() 
+    options = {
+        "notification": {
+            "title": "De pusc notifichescio",
+            "body": "Va che bella notifica che ti ho preso"
+        }
+    }
+    response = $fcm.send($registration_ids, options)
+    puts('Service response: ', response) #Leggere json response e verificare che valore di body -> success sia pari a 1 altrimenti errore
 end
 
 class Mail
@@ -14,7 +31,7 @@ class Mail
 end
 
 class LongobardiBACK < Sinatra::Base
-    newMail = false
+    #newMail = false
     mail = Mail.new()
     #Called by ESP8266 when the sensor is triggered
     post '/newMail' do
@@ -24,12 +41,13 @@ class LongobardiBACK < Sinatra::Base
         time = params['time']
         battery = params['battery']
         if (checkHashKey(hashkey, date, time, battery)) #The hashkey is checked to avoid data manipulation from unknown sources
-            newMail = true
+            #newMail = true
             mail.date = date
             mail.time = time
             mail.battery = battery
             response['status'] = 'ok'
             response['message'] = 'New mail registered'
+            sendPushNotification()
         else
             response['status'] = 'ko'
             response['message'] = 'An error occured, please try again'
@@ -38,18 +56,18 @@ class LongobardiBACK < Sinatra::Base
         response.to_json
     end
     #Called by longobardicAPP to check whether a new mail is available
-    get '/checkMailbox' do
-        response = {}
-        if (newMail) 
-            response['status'] = 'ok'
-            response['available_inbox'] = true
-            response['mail'] = JSON.parse(ActiveSupport::JSON.encode(mail)) #Hashed object converted in JSON
-            newMail = false
-        else 
-            response['status'] = 'ko'
-            response['available_inbox'] = false
-        end
-        content_type :json
-        response.to_json
-    end
+    # get '/checkMailbox' do
+    #     response = {}
+    #     if (newMail) 
+    #         response['status'] = 'ok'
+    #         response['available_inbox'] = true
+    #         response['mail'] = JSON.parse(ActiveSupport::JSON.encode(mail)) #Hashed object converted in JSON
+    #         newMail = false
+    #     else 
+    #         response['status'] = 'ko'
+    #         response['available_inbox'] = false
+    #     end
+    #     content_type :json
+    #     response.to_json
+    # end
 end

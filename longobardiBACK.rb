63a4c993fdc5aa4a44a1959769a7d632cc6d6f86
Eros Pingani <e.pingani3@gmail.com>
2021-01-15 14:12:18 +0100

    [DEV][BE] - Implementation refactored to be more scalable for new service that will be implemented

--- longobardiBACK.rb
+++ longobardiBACK.rb
@@ -11,11 +11,17 @@ $fcm = FCM.new('AAAA0ID3FSw:APA91bHveLJLI9jU_M3GQAN1ej-H8DjOFtxbFwGJ9kqWD2Ct8m1K
 #longobardicAPP registration token taken at opening from AStudio console (emulator, Pixel 3a)
 $registration_ids = ['e07mwn9JTCqIhKCkDFOr9g:APA91bEAwliA2uFbIP9pOFh8XlZ96R6S_jyQL1SapZK3SaxOhgQ6r2WGfuRoFRQRZhAmGbBdikc5vYChWwtpeMd9hIYCb3VsIjQxAYXsq1CLpUM-08eeHEsCX_lRNn_wVRe47F2shjJe', 'fOhLXUDkT9q5Sy_zA334I3:APA91bFfS8arvQgx4Po0DPIx26_ayYE_q8_4y37LLrj88-VgrFFIEce9fHmUNMozhSnSACWrE7sWvqCsWtMeRprKlfSLOQaWHgHht4UJ6BLV-L-YKkuXFw-PeANifxXB_CbFd8Cz3cBY']
 
-def checkHashKey(hashkey, date, time, battery)
-    return (Digest::SHA1.hexdigest KEY + date + time + battery).downcase == hashkey.downcase ? true : false
+#ArduinoInfo class that contains information about the device that called the service
+#Every call to the service has to pass these params (date, time, battery)
+class ArduinoInfo
+    attr_accessor :date, :time, :battery
 end
 
-def sendPushNotification(newMail) 
+def checkHashKey(hashkey, arduinoInfo)
+    return (Digest::SHA1.hexdigest KEY + arduinoInfo.date + arduinoInfo.time + arduinoInfo.battery).downcase == hashkey.downcase ? true : false
+end
+
+def sendPushNotification(notificationData) 
     options = {
         "priority": "high",
         "notification": {
@@ -23,33 +29,27 @@ def sendPushNotification(newMail)
             "body": "Va che bella notifica che ti ho preso"
         },
         "data": {
-            "mail": newMail
+            "content": notificationData
         }
     }
-    response = $fcm.send($registration_ids, options)
-    puts('Service response: ', response)
-end
-
-class Mail
-    attr_accessor :date, :time, :battery, :description
+    pushResponse = $fcm.send($registration_ids, options)
+    puts('Service response: ', pushResponse)
 end
 
 class LongobardiBACK < Sinatra::Base
-    mail = Mail.new()
+    arduinoInfo = ArduinoInfo.new()
     #Called by ESP8266 when the sensor is triggered
     post '/newMail' do
         response = {}
         hashkey = params['hashkey']
-        date = params['date']
-        time = params['time']
-        battery = params['battery']
-        if (checkHashKey(hashkey, date, time, battery)) #The hashkey is checked to avoid data manipulation from unknown sources
-            mail.date = date
-            mail.time = time
-            mail.battery = battery
+        arduinoInfo.date = params['date']
+        arduinoInfo.time = params['time']
+        arduinoInfo.battery = params['battery']
+        if (checkHashKey(hashkey, arduinoInfo)) #The hashkey is checked to avoid data manipulation from unknown sources
             response['status'] = 'ok'
             response['message'] = 'New mail registered'
-            response['mail'] = JSON.parse(ActiveSupport::JSON.encode(mail))
+            response['arduinoInfo'] = JSON.parse(ActiveSupport::JSON.encode(arduinoInfo))
+            response['notificationType'] = 1
             sendPushNotification(response)
         else
             response['status'] = 'ko'

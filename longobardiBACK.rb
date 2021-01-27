04725fa5971a75a4db0bd8d64c382e6eb2640646
Eros Pingani <e.pingani3@gmail.com>
2021-01-27 08:40:25 +0100

    [DEV][BE] - Added location param to arduinoInfo

--- longobardiBACK.rb
+++ longobardiBACK.rb
@@ -9,12 +9,12 @@ KEY = '196B5815CDE73CAE5CD7018359B851BBA754F80BB36FB7D50C1650F012496F70'
 #Firebase Cloud Messaging Server Key
 $fcm = FCM.new('AAAA0ID3FSw:APA91bHveLJLI9jU_M3GQAN1ej-H8DjOFtxbFwGJ9kqWD2Ct8m1KoMc9FoTS9QAeQo62nj_PJ6jmkEvlGH0nmPqSIwpo7bZaEN0tXNUWCXakM-D6G9UGhphAmB8KVAFd1AmO-09WeI2E')
 #longobardicAPP registration token taken at opening from AStudio console (emulator, Pixel 3a)
-$registration_ids = ['e07mwn9JTCqIhKCkDFOr9g:APA91bEAwliA2uFbIP9pOFh8XlZ96R6S_jyQL1SapZK3SaxOhgQ6r2WGfuRoFRQRZhAmGbBdikc5vYChWwtpeMd9hIYCb3VsIjQxAYXsq1CLpUM-08eeHEsCX_lRNn_wVRe47F2shjJe', 'fOhLXUDkT9q5Sy_zA334I3:APA91bFfS8arvQgx4Po0DPIx26_ayYE_q8_4y37LLrj88-VgrFFIEce9fHmUNMozhSnSACWrE7sWvqCsWtMeRprKlfSLOQaWHgHht4UJ6BLV-L-YKkuXFw-PeANifxXB_CbFd8Cz3cBY']
+$registration_ids = ['fj0OMkIhTgSgayKFCORl0Q:APA91bFKezxVnxeR_8xo2s0aP52hSVjNl5FS9-h5oH_t5L0X3BI8qH46xyWxUo5v3ULnUysRsiSxHDnxbpxdUVOVXEsCzIj7gzIXaNyOT75_d3lTIbShG6tlpvcDjDy0pcAO_n0Z6wNs', 'dgQXDnRXQUqF9P6f58Kj8B:APA91bH349vcDmBx4njTYqjo8GXcisCcz1WbYfQmUfU1vsubut--eN0gSAszTX_eTo_UFg5WAurHIRv0BVfrzY3u696Y8DV9JdctPZaUOX4hRSV93kbDtMBkC4JYrZ2n2YCS8Pk6wPpS']
 
 #ArduinoInfo class that contains information about the device that called the service
 #Every call to the service has to pass these params (date, time, battery)
 class ArduinoInfo
-    attr_accessor :date, :time, :battery
+    attr_accessor :date, :time, :battery, :location
 end
 
 def checkHashKey(hashkey, arduinoInfo)
@@ -45,6 +45,7 @@ class LongobardiBACK < Sinatra::Base
         arduinoInfo.date = params['date']
         arduinoInfo.time = params['time']
         arduinoInfo.battery = params['battery']
+        arduinoInfo.location = params['location'] == 'fara' ? 'Fara Gera d\'Adda' : 'Location'
         if (checkHashKey(hashkey, arduinoInfo)) #The hashkey is checked to avoid data manipulation from unknown sources
             response['status'] = 'ok'
             response['message'] = 'New mail registered'
